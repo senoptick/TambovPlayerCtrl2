fed61a62c57b3e53e17e78be82085f556a10c26f
senoptick <113616549+senoptick@users.noreply.github.com>
2026-03-09 20:32:32 +0300

    Update TambovPlayerCtrl.py

--- TambovPlayerCtrl.py
+++ TambovPlayerCtrl.py
@@ -9,18 +9,17 @@ import signal
 
 # ==================== НАСТРОЙКИ =====================
 
-BUTTON_LINES = [2, 8, 5, 6]  # wiringPi номера пинов
-LED_LINES = [16, 13, 10, 9]
+BUTTON_LINES = [2, 8]  # wiringPi номера пинов
+LED_LINES = [16, 13]
 
 SOUNDS = [
     "/content/1.wav",
-    "/content/2.wav",
-    "/content/3.wav",
-    "/content/4.wav"
+    "/content/2.wav"
 ]
 
 DEBOUNCE_TIME = 0.25
 LOCK_TIMEOUT = 1.5
+
 
 # ==================== ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ =====================
 
